4d18a62b2754751a15c943a2969ffd993e212168
lab raj thakurathi <lalitthakurathi@labs-MacBook-Air.local>
2019-05-06 00:53:50 -0700

    this should work

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,35 +3,97 @@ import Fade from "react-reveal/Fade";
 
 export default function Contact() {
   return (
-    <div className="contact-sec text-light p-3" id="contact">
-      <div className="text-center">
-        {" "}
-        <b>Contact</b>
-      </div>
+    <div>
+      <div className="contact-sec text-light p-3" id="contact">
+        <div className="text-center">
+          {" "}
+          <b>Contact</b>
+        </div>
 
-      <section>
-        <form method="post">
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Your Name: <input type="text" name="name" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your Email: <input type="email" name="email" />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
-        {/* <form
+        <section>
+          <form method="post">
+            <input type="hidden" name="form-name" value="contact" />
+            <div className="row">
+              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                <Fade bottom>
+                  <div className="my-4">
+                    <label htmlFor="firsName">First Name</label>
+                    <input
+                      type="text"
+                      name="firstName "
+                      id="fName"
+                      placeholder="*"
+                      className="form-control border-top-0 border-left-0 border-right-0 "
+                    />
+                  </div>
+                </Fade>
+                <Fade bottom>
+                  <div className="my-4">
+                    <label htmlFor="lastName">Last Name</label>
+                    <input
+                      type="text"
+                      name=" lastName "
+                      id="lName"
+                      placeholder="*"
+                      className="form-control border-top-0 border-left-0 border-right-0"
+                    />
+                  </div>
+                </Fade>
+                <Fade bottom>
+                  <div className="my-4">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="Email"
+                      name="email "
+                      id="email"
+                      placeholder="*"
+                      className="form-control border-top-0 border-left-0 border-right-0"
+                    />
+                  </div>
+                </Fade>
+              </div>
+              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                <Fade bottom>
+                  <div className="my-4">
+                    {" "}
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                      type=""
+                      name="phone"
+                      id="phone"
+                      placeholder="*"
+                      className="form-control border-top-0 border-left-0 border-right-0"
+                    />
+                  </div>
+                </Fade>
+                <Fade bottom>
+                  <div className="my-4">
+                    <label htmlFor="message">Your Messege</label>
+                    <textarea
+                      type="text-area"
+                      rows="5"
+                      cols="50"
+                      placeholder="*"
+                      className="form-control"
+                      name="message"
+                    />
+                  </div>
+                </Fade>
+                <div className="d-flex justify-content-end">
+                  <Fade bottom>
+                    <button
+                      className="btn btn-info btn-margin py-2 px-5 d-flex justify-content-end align-items-end"
+                      href="#projects"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </Fade>
+                </div>
+              </div>
+            </div>
+          </form>
+          {/* <form
           name="contact"
           action=""
           className="form-group "
@@ -120,7 +182,9 @@ export default function Contact() {
             </div>
           </div>
         </form> */}
-      </section>
+        </section>
+      </div>
+      <h1 className="text-center">Site Under Construction</h1>
     </div>
   );
 }
