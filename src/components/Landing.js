70c55739a440c3ef69d57919509834f90a9bdcea
lab raj thakurathi <lalitthakurathi@MacBook-Pro.local>
2019-10-02 19:34:22 -0700

    form validation

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,51 +1,55 @@
 import React from "react";
 import Particles from "react-particles-js";
 import { Parallax } from "react-scroll-parallax";
+import load1 from "./load.gif";
 
 export default function Landing() {
   return (
-    <div className="landing container-fluid">
-      <Particles
-        params={{
-          particles: {
-            number: {
-              value: 100,
-              density: {
-                enable: true,
-                value_area: 800
+    <div>
+      <div className="bg-light loader" />
+      <div className="landing container-fluid">
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 100,
+                density: {
+                  enable: true,
+                  value_area: 800
+                }
+              }
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                }
               }
             }
-          },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: "repulse"
-              }
-            }
-          }
-        }}
-        style={{
-          width: "50%"
-        }}
-      />
-      <div className="landing-overlay">
-        <div className="overlay">
-          <Parallax className="custom-class" y={[-35, 35]} tagOuter="figure">
-            <h2 className=" lead text-light ">
-              For your clean and user engaging UI,contact me.{" "}
-              <small>
-                Expect the best. Never settle for less, because I code for
-                PASSION.
-              </small>
-            </h2>
-            <a
-              className="btn btn-info py-2 my-3 px-5 btn-margin"
-              href="#projects"
-            >
-              See My Work
-            </a>
-          </Parallax>
+          }}
+          style={{
+            width: "50%"
+          }}
+        />
+        <div className="landing-overlay">
+          <div className="overlay">
+            <Parallax className="custom-class" y={[-35, 35]} tagOuter="figure">
+              <h2 className=" lead text-light ">
+                For your clean and user engaging UI,contact me.{" "}
+                <small>
+                  Expect the best. Never settle for less, because I code for
+                  PASSION.
+                </small>
+              </h2>
+              <a
+                className="btn btn-info py-2 my-3 px-5 btn-margin"
+                href="#projects"
+              >
+                See My Work
+              </a>
+            </Parallax>
+          </div>
         </div>
       </div>
     </div>
--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,259 +1,180 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 
-export default function Contact() {
-  return (
-    <div>
-      <div className="contact-sec text-light p-4 pb-5" id="contact">
-        <div className="text-center mb-4">
-          {" "}
-          <b>Contact</b>
-        </div>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
 
-        <section>
-          <form method="post">
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="row">
-              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="firstname">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      placeholder="*"
-                      className="form-control borderless"
-                    />
-                  </div>
-                </Fade>
+    this.state = {
+      fields: {},
+      errors: {}
+    };
+  }
 
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="*"
-                      className="form-control borderless"
-                    />
-                  </div>
-                </Fade>
-                <Fade bottom>
-                  <div className="my-4">
-                    {" "}
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                      type="tel"
-                      name="phone"
-                      placeholder="Optional"
-                      className="form-control borderless"
-                    />
-                  </div>
-                </Fade>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="message">Your Messege</label>
-                    <textarea
-                      type="text-area"
-                      rows="6"
-                      cols="50"
-                      placeholder="*"
-                      className="form-control"
-                      name="message"
-                    />
-                  </div>
-                </Fade>
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
 
-                <div className="d-flex justify-content-end">
-                  <Fade bottom>
-                    <button
-                      className="btn btn-info btn-margin py-2 mt-2 px-5 d-flex justify-content-end align-items-end"
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                  </Fade>
-                </div>
-              </div>
-            </div>
-          </form>
+    //Name
+    if (!fields["name"]) {
+      formIsValid = false;
+      errors["name"] = "Cannot be empty";
+    }
 
-          {/* <form method="post">
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="row">
-              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="firstname">First Name</label>
-                    <input
-                      type="text"
-                      name="firstname "
-                      id="firstname"
-                      placeholder="*"
-                      className="form-control border-top-0 border-left-0 border-right-0 "
-                    />
-                  </div>
-                </Fade>
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="lastname">Last Name</label>
-                    <input
-                      type="text"
-                      name=" lastname "
-                      id="lastname"
-                      placeholder="*"
-                      className="form-control border-top-0 border-left-0 border-right-0"
-                    />
-                  </div>
-                </Fade>
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email "
-                      id="email"
-                      placeholder="*"
-                      className="form-control border-top-0 border-left-0 border-right-0"
-                    />
-                  </div>
-                </Fade>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-                <Fade bottom>
-                  <div className="my-4">
-                    {" "}
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                      type="tel"
-                      name="phone"
-                      id="phone"
-                      placeholder="*"
-                      className="form-control border-top-0 border-left-0 border-right-0"
-                    />
-                  </div>
-                </Fade>
-                <Fade bottom>
-                  <div className="my-4">
-                    <label htmlFor="message">Your Messege</label>
-                    <textarea
-                      type="text-area"
-                      rows="5"
-                      cols="50"
-                      placeholder="*"
-                      className="form-control"
-                      name="message"
-                    />
-                  </div>
-                </Fade>
-                <div className="d-flex justify-content-end">
-                  <Fade bottom>
-                    <button
-                      className="btn btn-info btn-margin py-2 px-5 d-flex justify-content-end align-items-end"
-                      href="#projects"
-                      type="submit"
-                    >
-                      Submit
-                    </button>
-                  </Fade>
-                </div>
-              </div>
-            </div>
-          </form> */}
-          {/* <form
-          name="contact"
-          action=""
-          className="form-group "
-          method="POST"
-          data-netlify="true"
-        >
-          <div className="row">
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-              <Fade bottom>
-                <div className="my-4">
-                  <label htmlFor="first_name">First Name</label>
-                  <input
-                    type="text"
-                    name="first_name "
-                    id="fName"
-                    placeholder="*"
-                    className="form-control border-top-0 border-left-0 border-right-0"
-                  />
-                </div>
-              </Fade>
-              <Fade bottom>
-                <div className="my-4">
-                  <label htmlFor="last_name">Last Name</label>
-                  <input
-                    type="text"
-                    name="last_name "
-                    id="lName"
-                    placeholder="*"
-                    className="form-control border-top-0 border-left-0 border-right-0"
-                  />
-                </div>
-              </Fade>
-              <Fade bottom>
-                <div className="my-4">
-                  <label htmlFor="email">Email</label>
-                  <input
-                    type="text"
-                    name="email "
-                    id="email"
-                    placeholder="*"
-                    className="form-control border-top-0 border-left-0 border-right-0"
-                  />
-                </div>
-              </Fade>
-            </div>
-            <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
-              <Fade bottom>
-                <div className="my-4">
-                  {" "}
-                  <label htmlFor="phone">Phone</label>
-                  <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    placeholder="*"
-                    className="form-control border-top-0 border-left-0 border-right-0"
-                  />
-                </div>
-              </Fade>
-              <Fade bottom>
-                <div className="my-4">
-                  <label htmlFor="messege">Your Messege</label>
-                  <textarea
-                    type="text-area"
-                    rows="5"
-                    cols="50"
-                    placeholder="*"
-                    className="form-control"
-                    name="messege"
-                  />
-                </div>
-              </Fade>
+    if (typeof fields["name"] !== "undefined") {
+      if (!fields["name"].match(/^[a-zA-Z]+$/)) {
+        formIsValid = false;
+        errors["name"] = "Only letters";
+      }
+    }
+    //messege
+    if (!fields["messege"]) {
+      formIsValid = false;
+      errors["messege"] = "Cannot be empty";
+    }
 
-              <div className="d-flex justify-content-end">
-                <Fade bottom>
-                  <button
-                    className="btn btn-info btn-margin py-2 px-5 d-flex justify-content-end align-items-end"
-                    href="#projects"
-                    type="submit"
-                    value="Send"
-                  >
-                    Submit
-                  </button>
-                </Fade>
-              </div>
-            </div>
+    //Email
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    if (typeof fields["email"] !== "undefined") {
+      let lastAtPos = fields["email"].lastIndexOf("@");
+      let lastDotPos = fields["email"].lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          fields["email"].indexOf("@@") == -1 &&
+          lastDotPos > 2 &&
+          fields["email"].length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        errors["email"] = "Email is not valid";
+      }
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  contactSubmit(e) {
+    e.preventDefault();
+
+    if (this.handleValidation()) {
+      console.log("Form submitted");
+    } else {
+      console.log("Form has errors.");
+    }
+  }
+
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+  render() {
+    return (
+      <div>
+        <div className="contact-sec text-light p-4 pb-5" id="contact">
+          <div className="text-center mb-4">
+            {" "}
+            <b>Contact</b>
           </div>
-        </form> */}
-        </section>
+
+          <section>
+            <form
+              action="/"
+              method="post"
+              onSubmit={this.contactSubmit.bind(this)}
+            >
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="row">
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                  <Fade bottom>
+                    {
+                      <div className="my-4">
+                        <label htmlFor="firstname">Name</label>
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder={this.state.errors["name"]}
+                          className="form-control borderless"
+                          onChange={this.handleChange.bind(this, "name")}
+                          value={this.state.fields["name"]}
+                          ref="name"
+                        />
+                      </div>
+                    }
+                  </Fade>
+
+                  <Fade bottom>
+                    <div className="my-4">
+                      <label htmlFor="email">Email</label>
+
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder={this.state.errors["email"]}
+                        className="form-control borderless"
+                        onChange={this.handleChange.bind(this, "email")}
+                        value={this.state.fields["email"]}
+                        refs="email"
+                      />
+                    </div>
+                  </Fade>
+                  <Fade bottom>
+                    <div className="my-4">
+                      {" "}
+                      <label htmlFor="phone">Phone("optional")</label>
+                      <input
+                        type="tel"
+                        name="phone"
+                        className="form-control borderless"
+                      />
+                    </div>
+                  </Fade>
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6">
+                  <Fade bottom>
+                    <div className="my-4">
+                      <label htmlFor="message">Your Messege</label>
+                      <textarea
+                        type="text-area"
+                        rows="6"
+                        cols="50"
+                        className="form-control"
+                        name="message"
+                        placeholder={this.state.errors["messege"]}
+                        onChange={this.handleChange.bind(this, "messege")}
+                        value={this.state.fields["messege"]}
+                        ref="messege"
+                      />
+                    </div>
+                  </Fade>
+
+                  <div className="d-flex justify-content-end">
+                    <Fade bottom>
+                      <button
+                        className="btn btn-info btn-margin py-2 mt-2 px-5 d-flex justify-content-end align-items-end"
+                        type="submit"
+                      >
+                        Submit
+                      </button>
+                    </Fade>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+        <h1 className="text-center">Site Under Construction</h1>
       </div>
-      <h1 className="text-center">Site Under Construction</h1>
-    </div>
-  );
+    );
+  }
 }
+export default Contact;
